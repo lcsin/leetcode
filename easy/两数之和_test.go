bf4152559d2971963173a82238b20e8f5358bd15
lcsin <1847@qq.com>
2023-11-07 09:50:27 +0800

    new

--- easy/两数之和_test.go
+++ easy/两数之和_test.go
@@ -6,10 +6,23 @@ import (
 )
 
 func Test(t *testing.T) {
-	nums := []int{3, 3}
-	target := 6
+	nums := []int{2, 7, 11, 15}
+	target := 9
 
-	fmt.Println(twoSum(nums, target))
+	fmt.Println(twoSum1(nums, target))
+}
+
+func twoSum1(nums []int, target int) []int {
+	m := make(map[int]int)
+
+	for i, v := range nums {
+		if idx, ok := m[target-v]; ok {
+			return []int{idx, i}
+		}
+		m[v] = i
+	}
+
+	return nil
 }
 
 func twoSum(nums []int, target int) []int {
