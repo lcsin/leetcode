e29044d36cd5b3aa60b41951adafd1f3593032fa
lcsin <49903755+lcsin@users.noreply.github.com>
2024-03-21 18:32:11 +0800

    easy

---
+++ easy/283.移动零_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMoveZeroes(t *testing.T) {
+	num := []int{0, 1, 0, 3, 12}
+	moveZeroes(num)
+	fmt.Println(num)
+}
+
+func moveZeroes(nums []int) {
+	cnt := 0
+	for i, num := range nums {
+		if num == 0 {
+			cnt++
+		} else {
+			nums[i-cnt] = num
+		}
+	}
+	for i := 1; i <= cnt; i++ {
+		nums[len(nums)-i] = 0
+	}
+}
