ecfbdda574a8d54a746310a09a35c21332112906
lcsin <1847@qq.com>
2024-03-28 18:38:08 +0800

    new

--- easy/linknode/160.相交链表_test.go
+++ easy/linknode/160.相交链表_test.go
@@ -1,12 +1,35 @@
 package linknode
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestGetIntersectionNode(t *testing.T) {
+	headA := GenListNode([]int{1})
+	headB := GenListNode([]int{1})
+	//headC := GenListNode([]int{2, 4})
+	//headA.Next.Next = headC
+	//headB.Next = headC
+	PrintNode(headA)
+	PrintNode(headB)
 
+	node := getIntersectionNode(headA, headB)
+	fmt.Println(node)
 }
 
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	if headA.Next == nil && headB.Next == nil {
+		return nil
+	}
+
+	for i := headA; i != nil; i = i.Next {
+		for j := headB; j != nil; j = j.Next {
+			if i.Next == j.Next {
+				return i.Next
+			}
+		}
+	}
 
 	return nil
 }
