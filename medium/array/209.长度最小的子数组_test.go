fbecd54d57a1be2e88a2f07937e988e542aa5460
lcsin <1847@qq.com>
2024-04-10 18:28:42 +0800

    new

---
+++ medium/array/209.长度最小的子数组_test.go
@@ -0,0 +1,16 @@
+package array
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMinSubArrayLen(t *testing.T) {
+	nums := []int{2, 3, 1, 2, 4, 3}
+	fmt.Println(minSubArrayLen(7, nums))
+}
+
+func minSubArrayLen(target int, nums []int) int {
+
+	return 0
+}
