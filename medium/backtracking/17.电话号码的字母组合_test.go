fdbdf25ec02b757fd0fdc59d47bdd7c2cf313ff6
lcsin <1847@qq.com>
2024-04-18 13:17:35 +0800

    new

--- medium/backtracking/17.电话号码的字母组合_test.go
+++ medium/backtracking/17.电话号码的字母组合_test.go
@@ -7,15 +7,18 @@ import (
 )
 
 func TestLetterCombinations(t *testing.T) {
-	fmt.Println(letterCombinations("23"))
+	fmt.Println(letterCombinations(""))
 }
 
 var letters []string
 var letter string
-var keymap = []string{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}
+var keymap = []string{"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}
 
 func letterCombinations(digits string) []string {
 	letters = make([]string, 0)
+	if len(digits) == 0 {
+		return letters
+	}
 	backtrackingLetterCombinations(digits, 0)
 	return letters
 }
@@ -26,9 +29,10 @@ func backtrackingLetterCombinations(digits string, index int) {
 		return
 	}
 
-	for i := 0; i < len(digits); i++ {
-		idx, _ := strconv.Atoi(string(digits[i]))
-		letter += string(keymap[idx][index])
+	idx, _ := strconv.Atoi(string(digits[index]))
+	strs := keymap[idx-2]
+	for i := 0; i < len(strs); i++ {
+		letter += string(strs[i])
 		backtrackingLetterCombinations(digits, index+1)
 		letter = letter[:len(letter)-1]
 	}
